e12da526396b0666447c874895dc6394798e4152
Samuel <samus30@gmail.com>
2020-01-11 15:17:28 -0300

    Codigo inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import { Button } from 'reactstrap';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      <Button>teste</Button>
+    </div>
+  );
+}
+
+export default App;
